b6f3cc005ec6b00d0e3aee721f24cbe6f8648005
maryson1 <marysonemuekpere@gmail.com>
2024-12-13 17:54:36 +0100

    fixes, ieterations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { CreatecustomerComponent } from './pages/createcustomer/createcustomer.component';
+import { EditCustomerComponent } from './pages/edit-customer/edit-customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'customer',
+    pathMatch: 'full'
+  },
+  {
+    path: 'customer',
+    component: CustomerComponent
+  },
+  {
+    path: 'createcustomer',
+    component: CreatecustomerComponent
+  },
+  {
+    path: 'customer/edit/:id',
+    component: EditCustomerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialModule } from './shared/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CreatecustomerComponent } from './pages/createcustomer/createcustomer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EditCustomerComponent } from './pages/edit-customer/edit-customer.component';
+import { SearchCustomerComponent } from './pages/search-customer/search-customer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    CreatecustomerComponent,
+    EditCustomerComponent,
+    SearchCustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
